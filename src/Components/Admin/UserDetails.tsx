83ef94b7d5df7ec3dc652cf6d04d1032e125f52a
Cato Skogholt <cskogholt@gmail.com>
2018-08-12 11:19:38 +0200

    * reactstrap++

--- src/Components/Admin/UserDetails.tsx
+++ src/Components/Admin/UserDetails.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import { RouteComponentProps, Redirect } from 'react-router';
 
 import { api } from '../../Common/ApiService';
-import { IUser } from '../../Models/User';
+import { IUser, UserTypes } from '../../Models/User';
 import { Form, FormGroup, Input, Label, FormFeedback, Button } from 'reactstrap';
+import { ChangeEvent } from 'react';
+import { Link } from 'react-router-dom';
 
 interface IProps extends RouteComponentProps<any> {
 
@@ -47,45 +49,63 @@ export class UserDetails extends React.Component<IProps, IState> {
             return <Redirect to={'/users'} />
         }
 
-        const user = this.state.user;
+        const { user } = this.state;
+        const optionKeys = Object.keys(UserTypes)
+            .filter(p => typeof UserTypes[p as any] === "number");
+
         return (
             <div className="m-4">
                 <h2>Edit details for: {user.name}</h2>
+                <Link className="mb-4" to={'/users'}>Back to list</Link>
                 <Form className="needs-validation was-validated" noValidate={true}>
                     <FormGroup>
                         <Label for="name">Name</Label>
-                        <Input type="text" name="name" id="name" placeholder="Name" />
+                        <Input type="text" name="name" id="name" placeholder="Name"
+                            value={user.name}
+                            onChange={this.onFieldValueChange} />
                         <FormFeedback valid={false}>A name is required</FormFeedback>
                     </FormGroup>
                     <FormGroup>
                         <Label for="username">Username</Label>
-                        <Input type="email" name="username" id="username" placeholder="Username" />
+                        <Input type="email" name="username" id="username" placeholder="Username"
+                            value={user.username}
+                            onChange={this.onFieldValueChange} />
                         <FormFeedback valid={false}>A username is required</FormFeedback>
                     </FormGroup>
                     <FormGroup>
                         <Label for="password">Password</Label>
-                        <Input type="password" name="password" id="password" placeholder="Password" />
+                        <Input type="password" name="password" id="password" placeholder="Password"
+                            value={user.password}
+                            onChange={this.onFieldValueChange} />
                         <FormFeedback valid={false}>A password is required</FormFeedback>
                     </FormGroup>
                     <FormGroup>
                         <Label for="type">Type</Label>
-                        <Input type="text" name="type" id="type" placeholder="Type" />
+                        <Input type="select" name="type" id="type" placeholder="Type"
+                            value={user.type}
+                            onChange={this.onFieldValueChange}>
+                            {optionKeys.map((key, idx) =>
+                                <option key={idx} value={UserTypes[key]}>{key}</option>
+                            )}
+                        </Input>
                         <FormFeedback valid={false}>A type is required</FormFeedback>
                     </FormGroup>
                     <FormGroup>
-                        <Button className="btn btn-primary" label="Save" onClick={this.onSave} />
+                        <Button className="btn btn-primary" label="Save" onClick={this.onSave}>
+                            Save
+                        </Button>
                     </FormGroup>
                 </Form>
             </div>
         );
     }
 
-    private onFieldValueChange = (fieldName: string, value: string) => {
+    private onFieldValueChange = (event: ChangeEvent<HTMLInputElement>) => {
         const nextState = {
             ...this.state,
             user: {
                 ...this.state.user,
-                [fieldName]: value
+                [event.target.name]: event.target.value
             }
         };
         this.setState(nextState);
