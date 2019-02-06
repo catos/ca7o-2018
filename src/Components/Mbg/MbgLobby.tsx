42f9731988b9d557c9da96185762aca5737153d0
Cato Skogholt <cskogholt@gmail.com>
2019-02-06 22:18:39 +0100

    mbg

--- src/Components/Mbg/MbgLobby.tsx
+++ src/Components/Mbg/MbgLobby.tsx
@@ -3,9 +3,15 @@ import * as React from "react";
 import { AppConfig } from "src/AppConfig";
 import { SocketClientService, ISocketEvent } from "src/Common/SocketClientService";
 
+interface IMbgPlayer {
+    socketId: string;
+    name: string;
+}
+
 interface IState {
-    clients: string[],
-    rooms: string[]
+    name: string,
+    players: IMbgPlayer[],
+    rooms: string[],
 }
 
 export class MbgLobby extends React.Component<{}, IState> {
@@ -16,15 +22,17 @@ export class MbgLobby extends React.Component<{}, IState> {
 
         this.socketService = new SocketClientService(`${AppConfig.serverUrl}/mbg`),
 
-        this.state = {
-            clients: [],
-            rooms: []
-        }
+            this.state = {
+                name: '',
+                players: [],
+                rooms: []
+            }
     }
 
     public componentDidMount() {
-        this.socketService.eventHandlers.push({ type: 'get-clients', handle: this.onGetClients })
+        this.socketService.eventHandlers.push({ type: 'get-players', handle: this.onGetPlayers })
         this.socketService.eventHandlers.push({ type: 'get-rooms', handle: this.onGetRooms })
+        this.socketService.emit('get-players');
         this.socketService.emit('get-rooms');
     }
 
@@ -37,12 +45,19 @@ export class MbgLobby extends React.Component<{}, IState> {
             <div>
                 <h1>MbgLobby!</h1>
 
+                <div>
+                    <div className="lead">What is your name ?</div>
+                    <input type="text" placeholder="Enter your name" value={this.state.name} onChange={this.onNameChange} />
+                    <button className="btn btn-primary" onClick={this.join}>Join</button>
+                </div>
+
                 <h2>Clients</h2>
                 <ul>
-                    {this.state.clients.map((client, idx) =>
-                        <li key={idx}>{client}</li>
+                    {this.state.players.map((player, idx) =>
+                        <li key={idx}>{player.socketId} - {player.name}</li>
                     )}
                 </ul>
+
                 <h2>Rooms</h2>
                 <ul>
                     {this.state.rooms.map((room, idx) =>
@@ -53,8 +68,18 @@ export class MbgLobby extends React.Component<{}, IState> {
         );
     }
 
-    private onGetClients = (event: ISocketEvent) => {
-        this.setState({ clients: event.value });
+    private onNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {        
+        this.setState({ name: event.target.value });
+    }
+
+    private join = () => {
+        this.state.name.length
+            ? this.socketService.emit('join', this.state.name)
+            : console.log('Please enter a valid name');
+    }
+
+    private onGetPlayers = (event: ISocketEvent) => {
+        this.setState({ players: event.value });
     }
 
     private onGetRooms = (event: ISocketEvent) => {
